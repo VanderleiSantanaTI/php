4aceb8f3aa3e53a497cc501661e53fd3933980f1
Vanderlei Santana <109047525+VanderleiSantanaTI@users.noreply.github.com>
2022-12-12 20:50:16 -0300

    atualizou

--- estudonauta/includes/banco1.php
+++ estudonauta/includes/banco1.php
@@ -1,5 +1,5 @@
 <?php
-	 $banco = new mysqli("localhost", "479586", "19821982", "479586");
+	 $banco = new mysqli("localhost", "root", "", "nomebanco");
 	 if($banco->connect_errno){
 		 echo "<p> Encontrei um erro $banco->errno --> $banco->connect_error</p>";# mensage personalizada
 		 die(); #significa parar a execução se houver erro
