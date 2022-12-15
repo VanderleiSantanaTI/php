6b359194b488cbf1882ad15dae9519d606a3f7b4
Vanderlei Santana <109047525+VanderleiSantanaTI@users.noreply.github.com>
2022-12-15 15:56:14 -0300

    1.1

--- estudonauta/includes/banco1.php
+++ estudonauta/includes/banco1.php
@@ -1,5 +1,5 @@
 <?php
-	 $banco = new mysqli("localhost", "root", "", "nomebanco");
+	 $banco = new mysqli("localhost", "479586", "19821982", "479586");
 	 if($banco->connect_errno){
 		 echo "<p> Encontrei um erro $banco->errno --> $banco->connect_error</p>";# mensage personalizada
 		 die(); #significa parar a execução se houver erro
--- estudonauta/rodape.php
+++ estudonauta/rodape.php
@@ -1,6 +1,6 @@
 <?php
 	echo "<footer>";
-	echo "<p> Acessado por ". $_SERVER['REMOTE_ADDR'] ." em ". date('d/m/y') ."</p>";# se (d/m/y) for (D/M/Y) muda a formatação
+	echo "<p> Acessado por ". $_SERVER['REMOTE_ADDR'] ." em ". date('d/m/Y') ."</p>";# se (d/m/y) for (D/M/Y) muda a formatação
 	echo "<p>Desenvolvido por VSATECH &copy; 2022</p>";
 	echo "</footer>";
 	$banco->close();
